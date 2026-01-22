b1d0d301eab07baa63db05785cb7b0da2e3a17c0
Sita Vemuri <sitavemuri8@gmail.com>
2026-01-21 23:50:21 -0500

    clock code

--- sketch.js
+++ sketch.js
@@ -1,6 +1,12 @@
 // setup() is called once at page-load
 function setup() {
-    createCanvas(800, 600); // make an HTML canvas element width x height pixels
+    width = 1000;
+    height = 790;
+    createCanvas(width, height); // make an HTML canvas element width x height pixels
+
+    radius = 15;
+    bucketHeight = 150;
+    tubeHeight = 400;
 }
 
 // draw() is called 60 times per second
@@ -9,12 +15,30 @@ function draw() {
     let min = minute();
     let sec = second();
 
-    background(225);
-    textSize(32);
-    fill(180);
-    text(hr, 10, 30);
-    fill(100);
-    text(min, 10, 60);
-    fill(0);
-    text(sec, 10, 90);
+    background(255);
+    fill(156, 211, 219);
+
+    hour_height = map(hr, 0, 23, 0, bucketHeight);
+    min_height = map(min, 0, 59, 0, tubeHeight);
+
+    // hour
+    line(50, height, 50, height - bucketHeight);
+    line(width - 50, height, width - 50, height - bucketHeight);
+    rect(50, height - hour_height, width - 100, hour_height);
+    line(50, height - 1, width - 50, height - 1);
+
+    // second
+    circle(width / 2, map(sec, 0, 59, radius, height - min_height - 200 - radius), radius * 2);
+
+    // animation
+    if (sec == 0 && min == 0) {
+        translate(width, 0);
+        rotate(HALF_PI);
+    }
+
+    // minute
+    line(width / 2 - 50, height - 200, width / 2 - 50, height - 200 - tubeHeight);
+    line(width / 2 + 50, height - 200, width / 2 + 50, height - 200 - tubeHeight);
+    rect(width / 2 - 50, height - 200 - min_height, 100, min_height);
+    line(width / 2 - 50, height - 200, width / 2 + 50, height - 200);
 }
